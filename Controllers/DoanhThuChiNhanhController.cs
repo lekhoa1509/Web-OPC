0891a6d6e6d1d2f7551497d06c5fd0a7ea3b5c6e
Lê Khoa <lekhoa.lekhoa2@gmail.com>
2023-06-27 16:23:31 +0700

    upload code

--- Controllers/DoanhThuChiNhanhController.cs
+++ Controllers/DoanhThuChiNhanhController.cs
@@ -1,39 +1,37 @@
-﻿using DevExpress.Office.Import.OpenXml;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 using web4.Models;
+using System.Web.Mvc;
 
 namespace web4.Controllers
 {
-    public class BangKeHoaDonController : Controller
+    public class DoanhThuChiNhanhController : Controller
     {
         SqlConnection con = new SqlConnection();
         SqlCommand sqlc = new SqlCommand();
         SqlDataReader dt;
-        // GET: BangKeHoaDon
+        // GET: DoanhThuChiNhanh
         public ActionResult Index()
         {
             return View();
         }
+        
         public void connectSQL()
         {
             con.ConnectionString = "Data source= " + "118.69.109.109" + ";database=" + "SAP_OPC" + ";uid=sa;password=Hai@thong";
         }
-        public ActionResult bangkehoadon(Account Acc)
+
+        public ActionResult DoanhThuChiNhanh(Account Acc)
         {
             DataSet ds = new DataSet();
             connectSQL();
-            // Acc.Ma_DvCs_1 = Request.Cookies["MA_DVCS"].Value;
-            //Acc.UserName = Request.Cookies["UserName"].Value;
+            Acc.Ma_DvCs_1 = Request.Cookies["MA_DVCS"].Value;
             //string query = "exec usp_Vth_BC_BHCNTK_CN @_ngay_Ct1 = '" + Acc.From_date + "',@_Ngay_Ct2 ='"+ Acc.To_date+"',@_Ma_Dvcs='"+ Acc.Ma_DvCs_1+"'";
-            string Pname = "[usp_BKSaleOrder_SAP]";
-            Acc.UserName = Response.Cookies["UserName"].Value;
-
+            string Pname = "[usp_BKHoaDonBanHang_SAP]";
             using (SqlCommand cmd = new SqlCommand(Pname, con))
             {
                 cmd.CommandTimeout = 950;
@@ -47,20 +45,13 @@ namespace web4.Controllers
                     cmd.Parameters.AddWithValue("@_Tu_Ngay", Acc.From_date);
                     cmd.Parameters.AddWithValue("@_Den_Ngay", Acc.To_date);
                     cmd.Parameters.AddWithValue("@_ma_dvcs", Acc.Ma_DvCs_1);
-                    cmd.Parameters.AddWithValue("@_Ma_Dt", Acc.Ma_dt);
-                    cmd.Parameters.AddWithValue("@_Tinh_Trang", Acc.Tinh_Trang);
-                    cmd.Parameters.AddWithValue("@_username", Acc.UserName);
-                    
                     sda.Fill(ds);
 
                 }
             }
-
-
-            return View(ds);
-
+                return View(ds);
         }
-        public ActionResult bangkehoadon_Fill()
+      public ActionResult DoanhThuChiNhanh_Fill()
         {
             return View();
         }
---
+++ Controllers/EmptyRPController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web4.Controllers
+{
+    public class EmptyRPController : Controller
+    {
+        // GET: EmptyRP
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
