e555c948c3018346988c8a3177860df939d8d475
Lê Khoa <39689858+lekhoa1509@users.noreply.github.com>
2023-08-03 13:42:38 +0700

    Khoa Them DanhMuc

--- Controllers/DanhMucController.cs
+++ Controllers/DanhMucController.cs
@@ -58,7 +58,36 @@ namespace web4.Controllers
             }
             return View(ds);
         }
+
+        public ActionResult ViewDanhMucKHCN(Account Acc)
+        {
+            DataSet ds = new DataSet();
+            connectSQL();
+            Acc.Ma_DvCs_1 = Request.Cookies["MA_DVCS"].Value;
+            //string query = "exec usp_Vth_BC_BHCNTK_CN @_ngay_Ct1 = '" + Acc.From_date + "',@_Ngay_Ct2 ='"+ Acc.To_date+"',@_Ma_Dvcs='"+ Acc.Ma_DvCs_1+"'";
+            string Pname = "[usp_LookupKH_SAP]";
+            using (SqlCommand cmd = new SqlCommand(Pname, con))
+            {
+                cmd.CommandTimeout = 950;
+
+                cmd.Connection = con;
+                cmd.CommandType = CommandType.StoredProcedure;
+                Acc.Ma_DvCs_1 = Request.Cookies["MA_DVCS"].Value;
+                using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                {
+                    cmd.Parameters.AddWithValue("@_ma_dvcs", Acc.Ma_DvCs_1 + "_01");
+                    sda.Fill(ds);
+
+                }
+            }
+            return View(ds);
+        }
         public ActionResult ViewDanhMucKH_Fill()
+        {
+            return View();
+        }
+
+        public ActionResult ViewDanhMucKH_FillCN()
         {
             return View();
         }
